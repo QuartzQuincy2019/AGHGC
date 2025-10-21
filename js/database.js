7c1d48fe40b36eaa233bdbee01558dedcde32f74
Quincy K. <79129150+QuartzQuincy2019@users.noreply.github.com>
2025-10-22 00:10:19 +0800

    2.4.4.2

--- js/database.js
+++ js/database.js
@@ -534,8 +534,9 @@ var CHARACTER_LIST = [
         { "zh-CN": "昔涟", "en": "Cyrene", "jp": "キュレネ" },
         { "exclusiveLc": "thislo3", "alias": ["大昔涟", "PhiLia093", "爱莉希雅", "Elysia", "elysia"], "party": Party.Amphoreus }),
     //3.8
-    // new Character("", 5, CombatType., Path.,
-    //{ "zh-CN": "", "en": "", "jp": "" }),
+    new Character("dahl", 5, CombatType.fire, Path.nihility,
+        { "zh-CN": "大丽花", "en": "The Dahlia", "jp": "ダリア" }, 
+        { "alias": ["康士坦丝", "Constance", "コンスタンス"], "party": Party.Cosmic })
 ];
 
 var LIGHTCONE_LIST = [
